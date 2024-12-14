1d17e0c9af6dad5889cd3b9c22dd4d9be896e4c4
lasi-creator <smatthewmorgan@yccc.edu>
2024-12-14 13:09:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Matthew Morgan
+    File Name: script.js
+    Date: 12/13/24
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('hero)').height($(window).height());
+});
